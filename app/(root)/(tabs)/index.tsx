e630601eb2131ccece178e16f013fd6bbd70ed70
Abdelrahman Hassan <abdelrahman.hassan@sbs-sa.net>
2025-01-12 18:17:59 +0200

    Enhance home screen with welcome message and update navigation links; adjust global CSS path and extend Tailwind CSS configuration with custom fonts and colors

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -9,9 +9,11 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}>
+      <Text className="font-bold text-5xl my-10 text-red-500">
+        Welcome to your Home key
+      </Text>
       <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
+      <Link href="/Explore">Explore</Link>
       <Link href="/properties/1">Property</Link>
     </View>
   );
